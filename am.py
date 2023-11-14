64ed438b01766b0428066838f2ed0afd26d866d7
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-15 00:56:11 +0530

    Create am.py

---
+++ am.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("cd && rm -rf AMFileShareBot && git clone https://github.com/AMBOTYT/AMFileShareBot && cd AMFileShareBot && python3 main.py")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
