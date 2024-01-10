f9d6a5af95770af3aac4504781b675be5d430a5c
AMOPBOT <134328176+AMOPBOT@users.noreply.github.com>
2024-01-10 23:03:51 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf filesharebot && git clone https://github.com/AbhiMod/filesharebot && cd filesharebot && python3 main.py")
+system ("cd && rm -rf filesharebot && git clone https://github.com/AMOPBOT/filesharebot && cd filesharebot && python3 main.py")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
